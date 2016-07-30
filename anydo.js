562ea838b12607071f9ae1e77fea7f1733d3ceab
barryhe <yuhaohe@usc.edu>
2016-07-30 01:00:25 -0700

    second commit

--- anydo.js
+++ anydo.js
@@ -14,7 +14,7 @@ var connection = require('express-myconnection');
 var mysql = require('mysql');
 
 //environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3002);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 
